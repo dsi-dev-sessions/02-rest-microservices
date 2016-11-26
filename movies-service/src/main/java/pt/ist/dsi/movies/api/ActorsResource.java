e894a44fb480f2272cfb31beee7ef0d19391602f
sergiofbsilva <sergiofbsilva@gmail.com>
2016-11-26 19:29:14 +0000

    Simplify code

--- movies-service/src/main/java/pt/ist/dsi/movies/api/ActorsResource.java
+++ movies-service/src/main/java/pt/ist/dsi/movies/api/ActorsResource.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -73,21 +73,21 @@ public class ActorsResource {
 //
 //    }
     
-    @GetMapping(value = "/{actor}", produces = { MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.APPLICATION_XML_VALUE })
+    @GetMapping(value = "/{actor}")
     public Actor get(@PathVariable Optional<Actor> actor) {
         return actor.orElseThrow(this::actorNotFound);
     }
     
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Actor create(@RequestBody IncomingActor incomingActor) {
-        if (incomingActor == null || incomingActor.getName() == null || incomingActor.getName().isEmpty()) {
+        if (StringUtils.isEmpty(incomingActor.getName())) {
             throw new MovieAPIException(HttpStatus.BAD_REQUEST, "Can't create actor because name is null.");
         }
         return actorService.create(incomingActor.getName());
     }
     
-    @PutMapping(value = "/{actor}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(value = "/{actor}")
     public Actor update(@PathVariable Optional<Actor> actor, @RequestBody IncomingActor incomingActor) {
         return actorService.update(actor.orElseThrow(this::actorNotFound), incomingActor.getName());
     }
